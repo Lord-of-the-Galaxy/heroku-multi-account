e8b956291cabde3d21e40887adeaab4e7b94f6bb
Lord of Galaxy <prathikdiwakar@gmail.com>
2020-07-01 13:10:22 +0530

    Create hma_sched.py
    Hopefully this works, not tested yet

---
+++ hma_sched.py
@@ -0,0 +1,35 @@
+import os
+import time
+
+import requests as req
+
+from hma_conf import MASTER_APP
+
+MASTER_API_URL = f"https://api.heroku.com/apps/{MASTER_APP}"
+
+MASTER_TOKEN = os.environ['MASTER_HEROKU_TOKEN']
+
+MASTER_API_HEADERS = {
+        "Accept": "application/vnd.heroku+json; version=3",
+        "Authorization": f"Bearer {MASTER_TOKEN}"
+        }
+
+API_PAYLOAD = {'command': "python hma_master.py", 'attach':false 'size': 'free', 'type': 'run', 'time_to_live': 450}
+
+def run_hma():
+    r = req.post(f"{MASTER_API_URL}/dynos", json=API_PAYLOAD, headers=MASTER_API_HEADERS)
+    if r.status_code != req.codes.created:
+        print("Could not start the dyno")
+        print(r.text)
+        return False
+    return True
+
+
+done = False
+while True:
+    if time.gmtime().tm_hour == 18:
+        if not done:
+            done = run_hma()
+    else:
+        done = False
+    time.sleep(900)
